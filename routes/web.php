7acd6a758f5302b625a45e30a6214b889609b7c0
Casto <castoraron255@gmail.com>
2025-10-05 21:56:17 +0300

    fixed apiissues

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::prefix('api')->middleware('auth:sanctum')->group(function () {
-    Route::get('/contacts', [ContactController::class, 'index']);
-    Route::post('/contacts', [ContactController::class, 'store']);
-    Route::get('/contacts/{contact}', [ContactController::class, 'show']);
-    Route::patch('/contacts/{contact}', [ContactController::class, 'update']);
-    Route::delete('/contacts/{contact}', [ContactController::class, 'destroy']);
-});
+
+Route::prefix('api')
+    ->middleware(['api', 'auth:sanctum'])
+    ->withoutMiddleware([VerifyCsrfToken::class])
+    ->group(function () {
+        Route::get('/contacts', [ContactController::class, 'index']);
+        Route::post('/contacts', [ContactController::class, 'store']);
+        Route::get('/contacts/{contact}', [ContactController::class, 'show']);
+        Route::patch('/contacts/{contact}', [ContactController::class, 'update']);
+        Route::delete('/contacts/{contact}', [ContactController::class, 'destroy']);
+    });
